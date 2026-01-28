eee31a9590155ea919f8732e91e5b5c0284f5e7b
Jun Yi <junyi@chapman.edu>
2026-01-27 20:49:49 -0800

    final version

--- maxscore.sh
+++ maxscore.sh
@@ -4,22 +4,24 @@
 # cpsc298 intro to unix
 
 SCORE=()
+MAX=0
 
-read -rp "Enter 5 scores: " 'SCORE[0]'
+echo "Enter 5 scores:"
+read -r "SCORE[0]"
 MAX=${SCORE[0]}
 
-for (( i=1; i < 5; i++))
+for (( i=1; i<5; i++ ))
 do
-    read -r 'SCORE[i]'
-        if [[ ${SCORE[i]} -gt $MAX ]]; then
+    read -r "SCORE[$i]"
+        if (( SCORE[i] > MAX )); then
             MAX=${SCORE[i]}
         fi
 done
 
-echo "The highest scpre is $MAX"
+echo "The highest score is $MAX"
 echo "The scores are:"
 
-for (( INDEX=0; INDEX < 5; INDEX++))
+for (( INDEX=0; INDEX<5; INDEX++ ))
 do
-    echo "${SCORE[INDEX]} differs from max by $((MAX-SCORE[INDEX]))"
+    echo "${SCORE[INDEX]} differs from max by $(( MAX - SCORE[INDEX] ))"
 done
